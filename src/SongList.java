378c49890c3759011dbba77e00c3d7a4b79abfb3
esther1m <esthermikhael23@gmail.com>
2023-11-29 09:25:15 +0000

    created print method for the array list

--- src/SongList.java
+++ src/SongList.java
@@ -19,4 +19,22 @@ public class SongList {
     public void removeSong (Song song) {
         songStore.remove(this.song);
     }
+
+//creating a method to find a song over a given number of plays
+
+    public void findSong (int numberPlays){
+        for (int count = 0; count < songStore.size(); count ++){
+            if (songStore.get(count).getPlayCount() == numberPlays){
+                System.out.println("Found a song with " + numberPlays + " : " + songStore.get(count).getName() + " by: " + songStore.get(count).getArtist());
+            }
+        }
+    }
+
+//creating a method to print every song stored in the list
+
+    public void printSongStore (){
+        for (int count = 0; count < songStore.size(); count++){
+            songStore.get(count).printSong();
+        }
+    }
 }
