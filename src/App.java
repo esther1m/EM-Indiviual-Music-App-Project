3dceb2b6c4f7d1fa5d70d50bbc0edf19be49e025
esther1m <esthermikhael23@gmail.com>
2023-11-29 14:25:21 +0000

    created and added songs to the arraylist

--- src/App.java
+++ src/App.java
@@ -1,5 +1,39 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+//creating song objects to add to the song list
+
+        Song greedy = new Song("greedy", "Tate McRae", 390809229);
+        Song redLights = new Song("Red Lights", "Stray Kids", 105269785);
+        Song bitp = new Song("Barefoot In The Park", "James Blake", 302569785);
+        Song dealer = new Song("Dealer", "Lana Del Ray", 526875348);
+        Song ss = new Song("Summertime Sadness", "Lana Del Ray", 1168578464);
+        Song syth = new Song("Say Yes To Heaven", "Lana Del Ray", 287307170);
+        Song cgbb = new Song("Can't Go Back Baby", "Troye Sivan", 8638292);
+        Song rush = new Song("Rush", "Troye Sivan", 18043369);
+        Song plastic = new Song("Alby Plastic", "Ruby", 6101986);
+        Song coverMe = new Song("Cover Me", "Stray Kids", 9788608);
+
+//creating an object of the SongList class so that I can call the methods for it
+
+        SongList songStore = new SongList();
+
+//adding all the song objects to the array list
+
+        songStore.addSong (greedy);
+        songStore.addSong (redLights);
+        songStore.addSong (bitp);
+        songStore.addSong (ss);
+        songStore.addSong (syth);
+        songStore.addSong (cgbb);
+        songStore.addSong (rush);
+        songStore.addSong (plastic);
+        songStore.addSong (coverMe);
+
+//printing the arraylist
+
+        songStore.printSongStore();
+        
+
     }
 }
--- src/SongList.java
+++ src/SongList.java
@@ -6,12 +6,17 @@ public class SongList {
     Song song;
     ArrayList <Song> songStore = new ArrayList <Song>();
 
+    public SongList () {
+        //this.song = song;
+        
+    }
+
 //initialised the ArrayList to add songs to, as well as telling the computer that the Song object is needed
 
 //creating a method to add a song to the arraylist
 
     public void addSong (Song song){
-        songStore.add(this.song);
+        songStore.add(song);
     }
 
 //creating a method to remove a song from the arraylist
